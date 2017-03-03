2b8d65fb96e554640c724430e323e020f0fa19a9
LittleFishies <shotlock020@gmail.com>
2017-03-03 13:47:12 -0800

    Week 7 Homework!!!! (Writing my own scripts)
    Uh, well I essentially Frankensteined together some code to make it so
    that whenever the tent was hit by the fired projectile (which I named
    "rock"), it would jump in the air by a changable amount and change
    color. Upon starting the game, the console will say "try hitting a tent"
    and every time you hit one it says "ow"

---
+++ Assets/campsite/impact.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class impact : MonoBehaviour {
+
+    private Rigidbody getPhysics;
+    public Renderer matttttt;
+    public float reactionSpeed = 25;
+    private string startingOut = "Try hitting a tent!";
+    private string hitHitHit = "ow,";
+
+	// Use this for initialization
+	void Start () {
+        print(startingOut);
+        getPhysics = GetComponent<Rigidbody>();
+        matttttt = GetComponent<Renderer>();
+    }
+	
+	// Update is called once per frame
+	void OnCollisionEnter (Collision collider) {
+        if (collider.gameObject.name == "rock(Clone)")
+            {
+            print(hitHitHit);
+            getPhysics.AddForce(Vector3.up * reactionSpeed);
+            Color randoColor = Random.ColorHSV();
+            matttttt.material.SetColor("_Color", randoColor);
+            }
+         }
+}
