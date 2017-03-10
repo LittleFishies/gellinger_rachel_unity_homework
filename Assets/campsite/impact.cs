92ff46244698ad1caca1c7b5088bda1b00c68d56
LittleFishies <shotlock020@gmail.com>
2017-03-10 14:08:27 -0800

    Changing the scripts some more???
    yep. Added another if statement that is /supposed/ to say that if the
    projectile (rock) hits something that isn't the tent sides, the game
    says "why though" and then destroys the object that was just hit.

--- Assets/campsite/impact.cs
+++ Assets/campsite/impact.cs
@@ -6,7 +6,7 @@ public class impact : MonoBehaviour {
 
     private Rigidbody getPhysics;
     public Renderer matttttt;
-    public float reactionSpeed = 25;
+    public float reactionSpeed = 500;
     private string startingOut = "Try hitting a tent!";
     private string hitHitHit = "ow,";
 
@@ -19,12 +19,19 @@ public class impact : MonoBehaviour {
 	
 	// Update is called once per frame
 	void OnCollisionEnter (Collision collider) {
-        if (collider.gameObject.name == "rock(Clone)")
+        if ((collider.gameObject.name == "rock(Clone)") && (gameObject.name == "tent side riggg" || gameObject.name == "tent side lef" || gameObject.name == "tent side lef (1)" || gameObject.name == "tent side riggg (1)"))
             {
             print(hitHitHit);
             getPhysics.AddForce(Vector3.up * reactionSpeed);
             Color randoColor = Random.ColorHSV();
             matttttt.material.SetColor("_Color", randoColor);
             }
-         }
+        // I have no clue why this isn't working like it should - everything is doing this, even the tent sides. 
+        // I have a hunch I'm messing up the operators, but I can't really think how. 
+        if ((collider.gameObject.name == "rock(Clone)") && (gameObject.name != "tent side riggg" || gameObject.name != "tent side lef" || gameObject.name != "tent side lef (1)" || gameObject.name != "tent side riggg (1)"))
+            {
+            print("why though");
+            Destroy(gameObject);
+            }
+        }
 }
