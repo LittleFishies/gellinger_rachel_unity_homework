a7f5973d7be2d9239e86e279678713f12bb7fccc
LittleFishies <shotlock020@gmail.com>
2017-03-30 18:39:56 -0700

    Adding Sound!
    Added a handful of sound effects to the scene. Made a script that only
    plays certain sounds when the object hits something. Changed base
    footstep noises to match the environment. Most/all sound effects gotten
    from Sound Effects Public Domain on YouTube

---
+++ Assets/campsite/firewoodSound.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class firewoodSound : MonoBehaviour {
+    
+    private bool falling;
+    private AudioSource wood;
+    
+    // Use this for initialization
+    void Start () {
+        wood = GetComponent<AudioSource>(); 
+        falling = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+ 
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        falling = true;
+        if (falling == true)
+        {
+            wood.Play();
+        }
+    }
+}
